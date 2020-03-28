5c3b1c4d3d159e4964b90acda79597910dc9eb35
Harvey Sanders <sanders.harvey@gmail.com>
2020-03-27 22:47:46 -0500

    Init

---
+++ src/App.js
@@ -0,0 +1,210 @@
+import React from "react";
+import styled from "styled-components";
+import { useTable, useRowSelect } from "react-table";
+
+import makeData from "./makeData";
+
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`;
+
+const IndeterminateCheckbox = React.forwardRef(
+  ({ indeterminate, ...rest }, ref) => {
+    const defaultRef = React.useRef();
+    const resolvedRef = ref || defaultRef;
+
+    React.useEffect(() => {
+      resolvedRef.current.indeterminate = indeterminate;
+    }, [resolvedRef, indeterminate]);
+
+    return (
+      <>
+        <input type="checkbox" ref={resolvedRef} {...rest} />
+      </>
+    );
+  }
+);
+
+function Table({ columns, data }) {
+  // Use the state and functions returned from useTable to build your UI
+  // Number or null
+  const [rangeSelectInitialRow, setRangeSelectInitialRow] = React.useState(
+    false
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    selectedFlatRows,
+    state: { selectedRowIds }
+  } = useTable(
+    {
+      columns,
+      data
+    },
+    useRowSelect,
+    hooks => {
+      hooks.visibleColumns.push(columns => [
+        // Let's make a column for selection
+        {
+          id: "selection",
+          // The header can use the table's getToggleAllRowsSelectedProps method
+          // to render a checkbox
+          Header: ({ getToggleAllRowsSelectedProps }) => (
+            <div>
+              <IndeterminateCheckbox {...getToggleAllRowsSelectedProps()} />
+            </div>
+          ),
+          // The cell can use the individual row's getToggleRowSelectedProps method
+          // to the render a checkbox
+          Cell: context => {
+            const { row } = context;
+            const rowProps = { ...row.getToggleRowSelectedProps() };
+            const handleClick = e => {
+              if (e.ctrlKey || e.metaKey) {
+                  const first = context.selectedFlatRows.length
+                    ?  context.selectedFlatRows[0].index
+                  : null
+                  const last = context.row.index;
+                  for (const row of context.rows) {
+                    const selected = first <= row.index && row.index <= last;
+
+                    context.toggleRowSelected(row.index, selected);
+                    // console.log(row.index, selected);
+                  }
+                  console.log(first, last)
+              } else {
+                console.log("no modifier", rowProps);
+                context.toggleRowSelected(row.index, true);
+              }
+            };
+
+            return (
+              <div>
+                <IndeterminateCheckbox
+                  {...rowProps}
+                  // onChange={handleChange}
+                  onClick={handleClick}
+                />
+              </div>
+            );
+          }
+        },
+        ...columns
+      ]);
+    }
+  );
+
+  // Render the UI for your table
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.slice(0, 10).map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <p>Selected Rows: {Object.keys(selectedRowIds).length}</p>
+      <pre>
+        <code>
+          {JSON.stringify(
+            {
+              rangeSelectInitialRow,
+              selectedRowIds: selectedRowIds,
+              selectedFlatRows: selectedFlatRows.map(d => d.id)
+            },
+            null,
+            2
+          )}
+        </code>
+      </pre>
+    </>
+  );
+}
+
+function App() {
+  const columns = React.useMemo(() => [
+    {
+      Header: "First Name",
+      accessor: "firstName"
+    },
+    {
+      Header: "Last Name",
+      accessor: "lastName"
+    },
+
+    {
+      Header: "Age",
+      accessor: "age"
+    },
+    {
+      Header: "Visits",
+      accessor: "visits"
+    },
+    {
+      Header: "Status",
+      accessor: "status"
+    },
+    {
+      Header: "Profile Progress",
+      accessor: "progress"
+    }
+  ]);
+
+  const data = React.useMemo(() => makeData(10, 3), []);
+
+  return (
+    <Styles>
+      <Table columns={columns} data={data} />
+    </Styles>
+  );
+}
+
+export default App;
